ae9ae5576c79777c359291ba85347cbd37e7e55a
Evegarden <61862234+wutongyulu@users.noreply.github.com>
2023-10-20 13:48:33 +0800

    Delete devops/server directory

--- devops/server/apollo.config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  service: {
-    localSchemaFile: './src/graphql/schemas/schema.graphql',
-  },
-};
--- devops/server/src/graphql/schemas/index.ts
+++
@@ -1,6 +0,0 @@
-import { importSchema } from 'graphql-import';
-import { join } from 'path';
-
-const typeDefs = importSchema(join(__dirname, '/schema.graphql'));
-
-export default typeDefs;
